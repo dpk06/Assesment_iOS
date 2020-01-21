743b0443a092b8e3639c07753eb2510e29bd6955
Dpk <contact.deepak90@gmail.com>
2020-01-21 14:01:44 +0530

    create the UI- make a tableview & cell

---
+++ Assesment_iOS/Helper/CommonDataFile.swift
@@ -0,0 +1,9 @@
+//
+//  CommonDataFile.swift
+//  Assesment_iOS
+//
+//  Created by Developer on 21/01/20.
+//  Copyright © 2020 Developer. All rights reserved.
+//
+
+import Foundation
---
+++ Assesment_iOS/ViewController/DataTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DataTableViewCell.swift
+//  Assesment_iOS
+//
+//  Created by Developer on 21/01/20.
+//  Copyright © 2020 Developer. All rights reserved.
+//
+
+import UIKit
+
+class DataTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
